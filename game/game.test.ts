2c48c5f24dccce3bcb295e0255ca149bff59d932
Pavel Zubkov <zubkov_p@mail.ru>
2023-06-05 00:49:13 +0300

    Uncomment tests

--- game/game.test.ts
+++ game/game.test.ts
@@ -2,58 +2,58 @@ namespace $ {
 
 	$mol_test( {
 
-		// 'tube activation'() {
+		'tube activation'() {
 
-		// 	const game = new $hype_ballsort_game
-		// 	const tube1 = game.tubes()[0]
-		// 	const tube2 = game.tubes().at(-1)!
+			const game = new $hype_ballsort_game
+			const tube1 = game.tubes()[0]
+			const tube2 = game.tubes().at(-1)!
 
-		// 	$mol_assert_not(game.tube_active())
+			$mol_assert_not(game.tube_active())
 
-		// 	game.tube_click(tube1)
-		// 	$mol_assert_equal(tube1, game.tube_active())
+			game.tube_click(tube1)
+			$mol_assert_equal(tube1, game.tube_active())
 
-		// 	game.tube_click(tube2)
-		// 	$mol_assert_not(game.tube_active())
+			game.tube_click(tube2)
+			$mol_assert_not(game.tube_active())
 
-		// 	game.tube_click(tube2)
-		// 	$mol_assert_equal(tube2, game.tube_active())
+			game.tube_click(tube2)
+			$mol_assert_equal(tube2, game.tube_active())
 
-		// },
+		},
 
-		// 'ball moving'() {
+		'ball moving'() {
 
-		// 	const game = new $hype_ballsort_game
-		// 	const tube_filled = game.tubes()[ 0 ]
-		// 	const tube_empty = game.tubes().at( -1 )!
-		// 	const ball_moving = tube_filled.balls().at( -1 )!
+			const game = new $hype_ballsort_game
+			const tube_filled = game.tubes()[ 0 ]
+			const tube_empty = game.tubes().at( -1 )!
+			const ball_moving = tube_filled.balls().at( -1 )!
 
-		// 	game.tube_click( tube_filled )
-		// 	game.tube_click( tube_empty )
+			game.tube_click( tube_filled )
+			game.tube_click( tube_empty )
 
-		// 	$mol_assert_equal( tube_filled.balls().length, game.tube_size() - 1 )
-		// 	$mol_assert_not( tube_filled.balls().includes( ball_moving ) )
+			$mol_assert_equal( tube_filled.balls().length, game.tube_size() - 1 )
+			$mol_assert_not( tube_filled.balls().includes( ball_moving ) )
 
-		// 	$mol_assert_equal( tube_empty.balls().length, 1 )
-		// 	$mol_assert_ok( tube_empty.balls().includes( ball_moving ) )
+			$mol_assert_equal( tube_empty.balls().length, 1 )
+			$mol_assert_ok( tube_empty.balls().includes( ball_moving ) )
 
-		// },
+		},
 
-		// 'moves increment'() {
+		'moves increment'() {
 
-		// 	const game = new $hype_ballsort_game
-		// 	const tube_filled = game.tubes()[ 0 ]
-		// 	const tube_empty = game.tubes().at( -1 )!
+			const game = new $hype_ballsort_game
+			const tube_filled = game.tubes()[ 0 ]
+			const tube_empty = game.tubes().at( -1 )!
 
-		// 	game.tube_click( tube_filled )
-		// 	game.tube_click( tube_empty )
-		// 	$mol_assert_equal( game.moves(), 1 )
+			game.tube_click( tube_filled )
+			game.tube_click( tube_empty )
+			$mol_assert_equal( game.moves(), 1 )
 
-		// 	game.tube_click( tube_empty )
-		// 	game.tube_click( tube_filled )
-		// 	$mol_assert_equal( game.moves(), 2 )
+			game.tube_click( tube_empty )
+			game.tube_click( tube_filled )
+			$mol_assert_equal( game.moves(), 2 )
 
-		// },
+		},
 
 		// 'tube completed'() {
 
